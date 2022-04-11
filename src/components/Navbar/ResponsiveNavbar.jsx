1bc17f329dc030bef1f11f0720915cb941738625
manshad <manshadchangampalli010@gmail.com>
2022-04-11 20:56:35 +0530

    #added: the ccolor and the fonts are added

--- src/components/Navbar/ResponsiveNavbar.jsx
+++ src/components/Navbar/ResponsiveNavbar.jsx
@@ -5,8 +5,8 @@ const ResponsiveNavbar = ({ setNav, nav }) => {
     return (
         <div style={{ transform: nav && "translateY(0%)" }} className='res-navbar'>
             <svg onClick={() => setNav(false)} width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1.22168 1.2218L16.778 16.7782" stroke="white" stroke-width="2" stroke-linecap="round" />
-                <path d="M1.22168 16.7781L16.778 1.22179" stroke="white" stroke-width="2" stroke-linecap="round" />
+                <path d="M1.22168 1.2218L16.778 16.7782" stroke="black" stroke-width="2" stroke-linecap="round" />
+                <path d="M1.22168 16.7781L16.778 1.22179" stroke="black" stroke-width="2" stroke-linecap="round" />
             </svg>
             <Link to={"/"}> <p>Home</p></Link>
             <Link to={"/products"}><p>Products</p></Link>

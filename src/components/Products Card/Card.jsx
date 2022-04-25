0fcacfc3de8ba616a0c7a5f1ccb1bbefb9a1e297
manshad <manshadchangampalli010@gmail.com>
2022-04-25 13:38:28 +0530

    #added :the prodect name length checked and the hover fun added

--- src/components/Products Card/Card.jsx
+++ src/components/Products Card/Card.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Card = ({img,item}) => {
   return (
-    <div className='products-card'>
+    <div className={ item.length > 25 ? " show products-card" :"products-card" }>
         <img src={img} alt="" />
         <div className="name">
-            <p>{item}</p>
+          <p>{item.length > 25 ? item.slice( 0,25 )+ "..." : item}</p>
+        </div>
+        <div className="hoverName">
+          <p>{item}</p>
         </div>
     </div>
   )
--- src/pages/Products/ProductsDetails.js
+++ src/pages/Products/ProductsDetails.js
@@ -1,38 +1,63 @@
-import Prod1 from '../../Images/product/prod1.JPG'
-import Prod2 from '../../Images/product/prod7.JPG'
-import Prod3 from '../../Images/product/prod3.JPG'
-import Prod4 from '../../Images/product/prod4.JPG'
-import Prod5 from '../../Images/product/prod5.JPG'
-import Prod6 from '../../Images/product/prod6.JPG'
-import Prod7 from '../../Images/product/prod2.jpg'
+import Prod1 from '../../Images/product/Café latte Combo with Mini cinnamon reel-min.JPG'
+import Prod2 from '../../Images/product/Cappuccino Compo with Chocochi Cinnamon reel-min.JPG'
+import Prod3 from '../../Images/product/Ceremonial gifting Box-min.JPG'
+import Prod4 from '../../Images/product/Chocochi Box of 15-min.JPG'
+import Prod5 from '../../Images/product/Chocochi brand color Box-min.JPG'
+import Prod6 from '../../Images/product/Chocochi Brookie -min.JPG'
+import Prod7 from '../../Images/product/Chocochi Chocolate Lava Brownie -min.JPG'
+import Prod8 from '../../Images/product/Chocochi chocolate lava brownie with Vanilla Scoop-min.JPG'
+import Prod9 from '../../Images/product/Chocochi Take away Cup-min.JPG'
+import Prod10 from '../../Images/product/Choco mounded Almond filled Dates-min.JPG'
+import Prod11 from '../../Images/product/Choco mounded Cookies-min.JPG'
+import Prod12 from '../../Images/product/Choco mounded Dates-min.JPG'
  
 export const productsDetails = [
     {
-        name : "product1",
+        name : "Cafelatte & mini-cinnamo reel",
         img:Prod1
     },
     {
-        name : "product2",
+        name : "Cappuccino & Chocochi Cinnamon reel",
         img:Prod2
     },
     {
-        name : "product3",
+        name : "Ceremonial gifting box",
         img:Prod3
     },
     {
-        name : "product4",
+        name : "Chocochi Box of 15",
         img:Prod4
     },
     {
-        name : "product5",
+        name : "Chocochi brand color Box",
         img:Prod5
     },
     {
-        name : "product6",
+        name : "Chocochi Brookie",
         img:Prod6
     },
     {
-        name : "product7", 
+        name : "Chocochi Chocolate Lava Brownie", 
         img:Prod7
     },
+    {
+        name : "Chocochi chocolate lava brownie with Vanilla Scoop", 
+        img:Prod8
+    },
+    {
+        name : "Chocochi Take away Cup", 
+        img:Prod9
+    },
+    {
+        name : "Choco mounded Almond filled Dates", 
+        img:Prod10
+    },
+    {
+        name : "Choco mounded Cookies", 
+        img:Prod11
+    }, 
+    {
+        name : "Choco mounded Dates", 
+        img:Prod12
+    }, 
 ]

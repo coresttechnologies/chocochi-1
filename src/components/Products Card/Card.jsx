9be431715dc868febde5d46052c1a4d653b129fc
manshad <manshadchangampalli010@gmail.com>
2022-05-11 11:15:33 +0530

    #the bug fixed

--- src/components/Products Card/Card.jsx
+++ src/components/Products Card/Card.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-const Card = ({img,item}) => {
+const Card = ({ img, item }) => {
   return (
-    <div className={ item.length > 25 ? " show products-card" :"products-card" }>
-        <LazyLoadImage src={img} alt="" effect="blur"/>
-        <div className="name">
-          <p>{item.length > 25 ? item.slice( 0,25 )+ "..." : item}</p>
-        </div>
+    <div className={item.length > 25 ? " show products-card" : "products-card"}>
+      <LazyLoadImage src={img} alt="" effect="blur" />
+      <div className="name">
+        <p>{item.length > 25 ? item.slice(0, 25) + "..." : item}</p>
+      </div>
+      {
+        item.length > 25 &&
         <div className="hoverName">
           <p>{item}</p>
         </div>
+      }
     </div>
   )
 }
